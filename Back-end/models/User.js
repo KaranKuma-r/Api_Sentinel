78672badd7f98ffd7a8073175df593a0917eba96
Karan Kumar <kk5214662@gmail.com>
2026-02-04 21:32:12 +0530

    add user model schema

---
+++ Back-end/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format"]
+    },
+    password:{
+        type:String,
+        require:true,
+    },
+    
+    timestamps:true 
+})
