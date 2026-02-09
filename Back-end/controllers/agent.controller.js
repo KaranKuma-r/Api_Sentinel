49f1bebf12f085ac0cabcd285e1daacdac9b8003
Karan Kumar <kk5214662@gmail.com>
2026-02-09 16:45:47 +0530

    feat(agent): prevent duplicate agents per service and return existing agent key

--- Back-end/controllers/agent.controller.js
+++ Back-end/controllers/agent.controller.js
@@ -1,20 +1,33 @@
 const Agent = require("../models/Agent.model")
 const generatekey = require('../utils/generateskey')
 
-const createAgent =async(req,res)=>{
+const createAgent = async (req, res) => {
     try {
 
-        const {serviceName} =req.body
+        const { serviceName } = req.body
+
+        const existingAgent = await Agent.findOne({
+            userId: req.user.id,
+            serviceName
+        });
+
+        if (existingAgent) {
+            return res.status(200).json({
+                agentKey: existingAgent.agentKey,
+                message: "Agent already exists for this service name"
+            });
+        }
+
         const agent = await Agent.create({
-            userId:req.user.id,
+            userId: req.user.id,
             serviceName,
-            agentKey:generatekey()
+            agentKey: generatekey()
         })
-        
-        res.status(201).json({agentKey:agent.agentKey})
-    }catch(err){
-        res.status(500).json({message:err.message})
+
+        res.status(201).json({ agentKey: agent.agentKey })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
     }
 }
 
-module.exports=createAgent
+module.exports = createAgent
