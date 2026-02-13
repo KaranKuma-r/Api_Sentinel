74bb377407790253758103274f09be1b624c3b97
Karan Kumar <kk5214662@gmail.com>
2026-02-13 18:00:13 +0530

    change:Upadte AuthTokne name as a Agent

--- Back-end/middleware/agentAuth.middleware.js
+++ Back-end/middleware/agentAuth.middleware.js
@@ -5,7 +5,7 @@ const agentAuth = async (req, res,next) => {
     try {
         const authHeader = req.headers.authorization
 
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        if (!authHeader || !authHeader.startsWith("Agent ")) {
             return res.status(401).json({
                 message: "Agent key missing"
             });
