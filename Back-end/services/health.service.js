5c89d21ee5e6c33e146794e290321bd61c94ba56
Karan Kumar <kk5214662@gmail.com>
2026-02-13 17:56:10 +0530

    feat(health): add reusable health classification logic

--- Back-end/services/health.service.js
+++ Back-end/services/health.service.js
@@ -1,21 +1,28 @@
-function attachHealthStatus(endpoints) {
+function calculateHealth(data) {
 
-  return endpoints.map(ep => {
+  let status = "HEALTHY";
 
-    let status = "HEALTHY"
+  if (data.errorRate > 8) {
+    status = "UNHEALTHY";
+  }
+  else if (data.p95 > 1000 || data.avgLatency > 700) {
+    status = "UNHEALTHY";
+  }
+  else if (data.p95 > 600 || data.avgLatency > 400) {
+    status = "SLOW";
+  }
 
-    if (ep.errorRate > 5) {
-      status = "UNHEALTHY"
-    }
-    else if (ep.p95 > 800) {
-      status = "SLOW"
-    }
-
-    return {
-      ...ep,
-      status
-    }
-  })
+  return status;
 }
 
-module.exports = { attachHealthStatus }
+function attachHealthStatus(endpoints) {
+  return endpoints.map(ep => ({
+    ...ep,
+    status: calculateHealth(ep)
+  }));
+}
+
+module.exports = {
+  attachHealthStatus,
+  calculateHealth
+};
