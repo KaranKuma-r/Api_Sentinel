e260f5cbc43718e04fc029bd4e0bfdfecc0837d5
Karan Kumar <kk5214662@gmail.com>
2026-02-17 16:01:33 +0530

    feat(routes): add analytics routes for summary, endpoints, slow APIs, and timeseries drilldown

--- Back-end/routes/dashboard.routes.js
+++ Back-end/routes/dashboard.routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { getEndpoints,getSummary,getTimeSeriesData } = require('../controllers/dashboard.controller')
+const { getEndpoints,getSummary,getTimeSeriesData, slowEndpoints } = require('../controllers/dashboard.controller')
 const agentAuth = require('../middleware/agentAuth.middleware')
 
 router.get('/endpoints', agentAuth, getEndpoints)
 router.get('/summary', agentAuth, getSummary);   
 router.get("/timeseries",agentAuth,getTimeSeriesData)
+router.get("/slow", agentAuth, slowEndpoints);
+
 module.exports = router
