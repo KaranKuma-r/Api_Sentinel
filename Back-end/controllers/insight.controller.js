fa87742c78df0b79a438ca6f04468bba3752870e
Karan Kumar <kk5214662@gmail.com>
2026-02-26 22:04:59 +0530

    refactor(insights): update insight controller to use agentKey-based scoping

--- Back-end/controllers/insight.controller.js
+++ Back-end/controllers/insight.controller.js
@@ -1,37 +1,37 @@
 const { buildInsights } = require("../services/insight/insight.service");
-const {getTimeRange} = require("../utils/timeRange.util");
+const { getTimeRange } = require("../utils/timeRange.util");
 
-const  {getEndpointAggregation} = require("../services/aggregation.service");
+const { getEndpointAggregation } = require("../services/aggregation.service");
 
-const { getTimeSeries} = require("../services/timeseries.service");
+const { getTimeSeries } = require("../services/timeseries.service");
 
 exports.getInsights = async (req, res) => {
 
   try {
-    const { userId, serviceName } = req.agent;
+    const { agentKey } = req.agent
     const range = req.query.range || "1h";
 
-    const { startTime,unit } = getTimeRange(range);
+    const { startTime, unit } = getTimeRange(range);
     const endTime = new Date();
 
     const aggData = await getEndpointAggregation(
-      userId,
-      serviceName,
+      agentKey,
       startTime,
       endTime,
-      
+
     );
 
     const tsData = await getTimeSeries(
-      userId,
-      serviceName,
+      agentKey,
       startTime,
       endTime,
       unit
     );
 
     const insights = await buildInsights(aggData, tsData);
-    console.log(aggData,tsData);
+    console.log("AGG LENGTH:", aggData.length);
+    console.log("TS LENGTH:", tsData.length);
+    console.log("📊 INSIGHT API HIT");
 
 
     res.json({
@@ -43,7 +43,7 @@ exports.getInsights = async (req, res) => {
     console.error(err);
     res.status(500).json({
       success: false,
-      message: "Insight generation failed"
+      message: err.message
     });
   }
 
