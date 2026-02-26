a444ef14414f858b7bfe90944f4f6e7c627863e9
Karan Kumar <kk5214662@gmail.com>
2026-02-26 22:13:56 +0530

    feat(agent): add protected route to retrieve user agents

--- Back-end/routes/agent.routes.js
+++ Back-end/routes/agent.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 const auth = require("../middleware/auth.middleware")
-const createAgent=require("../controllers/agent.controller")
+const {createAgent,getAgents}=require("../controllers/agent.controller")
 
 router.post("/create",auth,createAgent)
-
+router.get('/serviceName',auth,getAgents)
 module.exports=router
