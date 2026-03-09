255c7a8cd2942e440df90515e7365b9d8fbf971f
Karan Kumar <kk5214662@gmail.com>
2026-03-09 18:24:53 +0530

    refactor(api): move dashboard API base URL to environment variable

--- Front-end/src/api/dasboardApi.js
+++ Front-end/src/api/dasboardApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/dashboard";
+const BASE_URL = import.meta.env.VITE_DASHBOARD_URL;
 
 export const getSummary = async(agentKey)=>{
      
